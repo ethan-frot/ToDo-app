c62540fab17542420539bcada480de50b5b41f0f
Ethan Frot <contact@ethanfrot.com>
2025-04-02 08:56:33 +0200

    ⚰️ Remove dead code

--- app/src/tests/unit/TodoItem.spec.tsx
+++ app/src/tests/unit/TodoItem.spec.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import TodoItem from "@/components/TodoItem/TodoItem";
 import { Status } from "@/types/todo.type";
 import "@testing-library/jest-dom";
